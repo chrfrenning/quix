7c2d6e2c5a7e43fe4b6caec622dbde5b9e7a0d35
mariuaas <mariuaas@ifi.uio.no>
2024-01-12 20:22:29 +0100

    add required packages and python version

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='quix',
-    version='0.1.1',
-    description='QUIck eXperiment',
-    licence='GNUv3',
-    packages=['quix', 'quix.run', 'quix.cfg', 'quix.data']
+    name = 'quix',
+    version = '0.1.2',
+    description = 'QUIck eXperiment',
+    licence = 'GNUv3',
+    packages = ['quix', 'quix.run', 'quix.cfg', 'quix.data'],
+    install_requires = [
+        'toml >= 0.10.2',
+        'numpydoc >= 1.6.0',
+        'PyYaml >= 6.0',
+        'torch >= 2.1.0',
+        'torchvision >= 0.16.0'
+    ]
 )
