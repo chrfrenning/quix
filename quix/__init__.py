3df19f43c3d6a720f77e6835ff0f21e2e6f89885
mariuaas <mariuaas@ifi.uio.no>
2024-01-12 13:58:52 +0100

    Add extra namespaces

--- quix/__init__.py
+++ quix/__init__.py
@@ -1,3 +1,9 @@
 from .run import Runner, AbstractRunner, single_node_launcher
 from .cfg import ModelConfig, DataConfig, OptimizerConfig, LogConfig, RunConfig
 from .proc import BatchProcessor
+from .log import (
+    LogCollator, AbstractLogger, ProgressLogger, LossLogger, LRLogger, GPULogger,
+    AccuracyLogger, DeltaTimeLogger
+)
+from .ema import ExponentialMovingAverage
+from .sched import CosineDecay
