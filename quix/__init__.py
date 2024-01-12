115dbb94ce74015da6e7ee600f6a5ff18bdb2c10
mariuaas <mariuaas@ifi.uio.no>
2024-01-12 12:16:41 +0100

    Fix annoying bug

--- quix/__init__.py
+++ quix/__init__.py
@@ -1,3 +1,3 @@
-from .run import Runner, AbstractRunner, single_node_launcher
-from .cfg import ModelConfig, DataConfig, OptimizerConfig, LogConfig, RunConfig
+from .run.runner import Runner, AbstractRunner, single_node_launcher
+from .cfg.config import ModelConfig, DataConfig, OptimizerConfig, LogConfig, RunConfig
 from .proc import BatchProcessor
